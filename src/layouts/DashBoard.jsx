e53db36cb8d79070e6182d5cd4e7402b18cd6576
joynalabedin05 <joynal05101993@gmail.com>
2023-06-09 12:32:43 +0600

    dashboard added

--- src/layouts/DashBoard.jsx
+++ src/layouts/DashBoard.jsx
@@ -1,8 +1,14 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { FaHome, FaBook, FaUser, FaUsers } from 'react-icons/fa';
+import useAdmin from "../hooks/useAdmin";
+import useInstructor from "../hooks/useInstructor";
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
 
 const DashBoard = () => {
-
+  const { user } = useContext(AuthContext);
+  const [isAdmin] = useAdmin();
+  const [isInstructor] = useInstructor();
   return (
     <div className="drawer lg:drawer-open my-10">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -15,12 +21,31 @@ const DashBoard = () => {
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 bg-orange-100">
           {/* Sidebar content here */}
-          <li><a>Sidebar Item 1</a></li>
-          <li>
-            <NavLink to='/dashboard/allusers'><FaUsers /> All Users</NavLink>
-          </li>
-          <li><a>Sidebar Item 2</a></li>
-          <li><a>Sidebar Item 2</a></li>
+          {
+            isAdmin ?
+              <>
+                <li>
+                  <NavLink to='/dashboard/allusers'><FaUsers /> All Users</NavLink>
+                </li>
+                <li><a>Sidebar Item 1</a></li>
+                <li><a>Sidebar Item 2</a></li>
+              </> :
+              isInstructor ?
+                <>
+
+                  <li><a>Sidebar Item 3</a></li>
+                  <li><a>Sidebar Item 4</a></li>
+                </> :
+
+                <>
+                  <li><a>Sidebar Item 5</a></li>
+                  <li><a>Sidebar Item 6</a></li>
+                </>
+          }
+
+
+
+
           <div className="divider"></div>
           <li><NavLink to='/'><FaHome /> Home</NavLink></li>
           <li><NavLink to='/instructor'><FaUser /> Instructors</NavLink></li>
